e646bf557bf414291ad60d9af59572b2e7a6af4a
Birusha Ndegeya <142086785+BirushaNdegeya@users.noreply.github.com>
2024-02-16 19:55:17 +0200

    update
    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,31 @@
 import React from 'react';
-
-import { 
-  AboutUs, 
-  Chef, 
-  FindUs, 
-  Footer, 
-  Gallery, 
-  Header, 
-  Intro, 
-  Laurels, 
-  SpecialMenu 
-} from './container';
+import AboutUs from './container/AboutUs/AboutUs';
+import Chef from './container/Chef/Chef';
+import FindUs from './container/Findus/FindUs';
+import Footer from './container/Footer/Footer';
+import Gallery from './container/Gallery/Gallery';
+import Header from './container/Header/Header';
+import Intro from './container/Intro/Intro';
+import Laurels from './container/Laurels/Laurels';
+import SpecialMenu from './container/Menu/SpecialMenu';
 import { Navbar } from './components';
 import './App.css';
 
-const App = () => (
-  <div>
-    <Navbar />
-    <Header />
-    <AboutUs />
-    <SpecialMenu />
-    <Chef />
-    <Intro />
-    <Laurels />
-    <Gallery />
-    <FindUs />
-    <Footer />
-  </div>
-);
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Header />
+      <AboutUs />
+      <SpecialMenu />
+      <Chef />
+      <Intro />
+      <Laurels />
+      <Gallery />
+      <FindUs />
+      <Footer />
+    </div>
+  );
+};
 
 export default App;
